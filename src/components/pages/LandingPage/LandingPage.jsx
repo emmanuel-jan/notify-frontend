d5213dbe3457ce872eaa67648a41a39230a5e42e
emmanuel-jan <emmanueljan80@gmail.com>
2023-01-08 19:13:12 +0300

    adding animations to the landing page

--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -22,6 +22,8 @@ import scale from "../../../assets/img/scale.svg";
 import email from "../../../assets/img/email.svg";
 import { DollarCircleOutlined } from "@ant-design/icons";
 import "./LandingPage.css";
+import { motion, useScroll } from "framer-motion";
+
 const { Header, Content, Footer } = Layout;
 const { Title, Text, Paragraph } = Typography;
 
@@ -50,6 +52,7 @@ const LandingPage = (props) => {
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+  const { scrollYProgress } = useScroll();
   return (
     <Layout className="thefont">
       <Header
@@ -61,27 +64,22 @@ const LandingPage = (props) => {
           backgroundColor: "#1890ff",
         }}
       >
-        <div
-          style={{
-            float: "left",
-            width: 120,
-            height: 31,
-            margin: "16px 24px 16px 0",
-            background: "rgba(255, 255, 255, 0.2)",
-          }}
-        ></div>
-        <Menu
-          className="thefont"
-          theme="dark"
-          mode="horizontal"
-          style={{
-            backgroundColor: "#1890ff",
-          }}
-          items={new Array(3).fill(null).map((_, index) => ({
-            key: String(index + 1),
-            label: `nav ${index + 1}`,
-          }))}
-        />
+        <Row justify="center">
+          <Col xs={24} sm={24} md={5} lg={5}>
+            <Title
+              className="thefont"
+              style={{ margin: "0", paddingTop: "10px", color: "white" }}
+              type="secondary"
+              level={2}
+            >
+              <motion.div initial={{y:-250}} animate={{y:0}} transition={{delay:0.5}}>
+              Notify
+              </motion.div>
+              
+            </Title>
+          </Col>
+          <Col align="center" xs={24} sm={24} md={15} lg={15}></Col>
+        </Row>
       </Header>
       <Content
         className="site-layout"
@@ -99,154 +97,185 @@ const LandingPage = (props) => {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              height:'100vh'
+              height: "100vh",
             }}
           >
-            <div>
-              <Text
-                className="thefont"
-                style={{ padding: "0px", margin: "0px", color: "#1890ff" }}
-              >
-                EMAIL DELIVERY. SIMPLIFIED.
-              </Text>
-              <Title
-                type="secondary"
-                className="thefont"
-                style={{ padding: "0px", margin: "0px" }}
-              >
-                Leader in trusted email delivery
-              </Title>
-              <Paragraph
-                className="thefont"
-                style={{
-                  padding: "0px",
-                  margin: "0px",
-                  fontSize: "18px",
-                  lineHeight: "1.2",
-                }}
-                type="secondary"
-              >
-                Our powerful cloud-based solution processes over 100 billion
-                emails/month for thousands of customers, allowing them to focus
-                on their business, instead of their email infrastructure.
-              </Paragraph>
-              <Space wrap style={{ marginTop: "10px" }}>
-                <Button className="thefont" type="primary" shape="round">
-                  Get Started
-                </Button>
-                <Button
+            <motion.div
+              initial={{ opacity: 0, y: -250 }}
+              animate={{
+                opacity: 1,
+                y: -10,
+              }}
+              transition={{delay:1.5, duration:0.5}}
+            >
+              <div>
+                <Text
                   className="thefont"
-                  type="primary"
-                  shape="round"
-                  onClick={showModal}
-                  ghost
+                  style={{ padding: "0px", margin: "0px", color: "#1890ff" }}
                 >
-                  Register
-                </Button>
-              </Space>
-              <Modal
-                title={<Title align="center" level={3} type="secondary">Your Details Will Help Us Set You Up!</Title>}
-                open={open}
-                onOk={handleOk}
-                confirmLoading={confirmLoading}
-                onCancel={handleCancel}
-              >
-                <Form
-                  name="basic"
-                  labelCol={{
-                    span: 8,
-                  }}
-                  wrapperCol={{
-                    span: 16,
-                  }}
-                  initialValues={{
-                    remember: true,
-                  }}
-                  onFinish={onFinish}
-                  onFinishFailed={onFinishFailed}
-                  autoComplete="off"
+                  EMAIL DELIVERY. SIMPLIFIED.
+                </Text>
+                <Title
+                  type="secondary"
+                  className="thefont"
+                  style={{ padding: "0px", margin: "0px" }}
                 >
-                  <Form.Item
-                    name="name"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your name!",
-                      },
-                    ]}
-                    align="center"
+                  Leader in trusted email delivery
+                </Title>
+                <Paragraph
+                  className="thefont"
+                  style={{
+                    padding: "0px",
+                    margin: "0px",
+                    fontSize: "18px",
+                    lineHeight: "1.2",
+                  }}
+                  type="secondary"
+                >
+                  Our powerful cloud-based solution processes over 100 billion
+                  emails/month for thousands of customers, allowing them to
+                  focus on their business, instead of their email
+                  infrastructure.
+                </Paragraph>
+                <Space wrap style={{ marginTop: "10px" }}>
+                  <Button className="thefont" type="primary" shape="round">
+                    Get Started
+                  </Button>
+                  <Button
+                    className="thefont"
+                    type="primary"
+                    shape="round"
+                    onClick={showModal}
+                    ghost
                   >
-                    <Input placeholder="John Doe" />
-                  </Form.Item>
-                  <Form.Item
-                    name="appname"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your appname!",
-                      },
-                    ]}
-                    align="center"
-                  >
-                    <Input placeholder="Enter Name of Your App" />
-                  </Form.Item>
-                  <Form.Item
-                    name="phone"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your phone number!",
-                      },
-                    ]}
-                    align="center"
-                  >
-                    <Input placeholder="Enter Your Phone Number" />
-                  </Form.Item>
-                  <Form.Item
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your email!",
-                      },
-                    ]}
-                    align="center"
-                  >
-                    <Input placeholder="Enter Your Emai;" />
-                  </Form.Item>
-
-                  <Form.Item
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your password!",
-                      },
-                    ]}
-                    align="center"
-                  >
-                    <Input.Password placeholder="Enter Your Password" />
-                  </Form.Item>
-
-                  <Form.Item
+                    Register
+                  </Button>
+                </Space>
+                <Modal
+                  title={
+                    <Title align="center" level={3} type="secondary">
+                      Your Details Will Help Us Set You Up!
+                    </Title>
+                  }
+                  open={open}
+                  onOk={handleOk}
+                  confirmLoading={confirmLoading}
+                  onCancel={handleCancel}
+                >
+                  <Form
+                    name="basic"
+                    labelCol={{
+                      span: 8,
+                    }}
                     wrapperCol={{
-                      offset: 8,
                       span: 16,
                     }}
+                    initialValues={{
+                      remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                   >
-                    {/* <Button type="primary" htmlType="submit">
+                    <Form.Item
+                      name="name"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your name!",
+                        },
+                      ]}
+                      align="center"
+                    >
+                      <Input placeholder="John Doe" />
+                    </Form.Item>
+                    <Form.Item
+                      name="appname"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your appname!",
+                        },
+                      ]}
+                      align="center"
+                    >
+                      <Input placeholder="Enter Name of Your App" />
+                    </Form.Item>
+                    <Form.Item
+                      name="phone"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your phone number!",
+                        },
+                      ]}
+                      align="center"
+                    >
+                      <Input placeholder="Enter Your Phone Number" />
+                    </Form.Item>
+                    <Form.Item
+                      name="email"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your email!",
+                        },
+                      ]}
+                      align="center"
+                    >
+                      <Input placeholder="Enter Your Emai;" />
+                    </Form.Item>
+
+                    <Form.Item
+                      name="password"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your password!",
+                        },
+                      ]}
+                      align="center"
+                    >
+                      <Input.Password placeholder="Enter Your Password" />
+                    </Form.Item>
+
+                    <Form.Item
+                      wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                      }}
+                    >
+                      {/* <Button type="primary" htmlType="submit">
                       Submit
                     </Button> */}
-                  </Form.Item>
-                </Form>
-              </Modal>
-            </div>
+                    </Form.Item>
+                  </Form>
+                </Modal>
+              </div>
+            </motion.div>
           </Col>
+
           <Col xs={24} sm={24} md={10} lg={10}>
+          <motion.div
+              initial={{ opacity: 0}}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{delay:1.5}}
+            >
             <img style={{ width: "100%" }} src={cover} alt="" />
+            </motion.div>
           </Col>
         </Row>
-        <Row gutter={[16, 24]} justify="center" style={{ display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <Row
+          gutter={[16, 24]}
+          justify="center"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Col xs={24} sm={24} md={20} lg={20}>
             <Col xs={24} sm={24} md={24} lg={24}>
               <Title
@@ -319,7 +348,15 @@ const LandingPage = (props) => {
             </Row>
           </Col>
         </Row>
-        <Row justify="center" style={{height:"100vh", display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <Row
+          justify="center"
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Col xs={24} sm={24} md={20} lg={20}>
             <Row gutter={16}>
               <Col align="center" xs={24} sm={24} md={24} lg={24}>
